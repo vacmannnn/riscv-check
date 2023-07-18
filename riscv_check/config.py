3f0ef7f428e77be413f3510fb54b15592e6be37b
Andrei <15161335+therain7@users.noreply.github.com>
2023-07-18 04:20:56 +0300

    feat: get app config from config.json

--- riscv_check/config.py
+++ riscv_check/config.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -11,8 +12,11 @@ class AppConfig:
 
 
 def get_config() -> AppConfig:
-    return AppConfig(
-        tests_dir_path=Path(),
-        compiler_path=Path(),
-        march="abc",
-    )
+    with open("config.json", "r") as f:
+        json_conf = json.load(f)
+
+        return AppConfig(
+            tests_dir_path=Path(json_conf["tests_dir_path"]),
+            compiler_path=Path(json_conf["compiler_path"]),
+            march=json_conf["compiler_march"],
+        )
