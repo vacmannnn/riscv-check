62967d42d207bd84d1519f85ec19c34d6c2188ef
Alexandr Lecomcev <opera9876@rambler.ru>
2023-07-21 17:08:13 +0300

    feat: impl Builder

--- riscv_check/checker/builder.py
+++ riscv_check/checker/builder.py
@@ -1,6 +1,14 @@
+import subprocess
 from dataclasses import dataclass
+from os import path
 from pathlib import Path
+from shutil import rmtree
+from tempfile import mkdtemp
 from typing import Iterable, Protocol
+
+
+class CompileError(Exception):
+    pass
 
 
 @dataclass
@@ -19,4 +27,29 @@ class Builder:
         self.compiler = compiler
 
     def build_to_asm(self, code: str, additional_args: Iterable[str]) -> str:
-        raise NotImplementedError
+        temp_dir = mkdtemp()
+
+        Path(temp_dir, "code.c").write_text(code)
+
+        try:
+            result = subprocess.run(
+                [self.compiler.compiler_path]
+                + list(self.compiler.compiler_args)
+                + list(additional_args)
+                + [
+                    "-S",
+                    "-o",
+                    path.join(temp_dir, "asm_code.s"),
+                    path.join(temp_dir, "code.c"),
+                ],
+                capture_output=True,
+                text=True,
+            )
+
+            if result.returncode != 0:
+                raise CompileError(result.stderr)
+
+            return Path(temp_dir, "asm_code.s").read_text()
+
+        finally:
+            rmtree(temp_dir)
