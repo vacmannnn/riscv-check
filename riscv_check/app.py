483e5f3659314d92f2f39748494fdb10718449d8
Andrei <15161335+therain7@users.noreply.github.com>
2023-07-27 15:28:08 +0300

    feat: handle test compilation errors

--- riscv_check/app.py
+++ riscv_check/app.py
@@ -1,6 +1,8 @@
 import asyncio
 from logging import Logger
-from typing import Iterable
+from typing import Iterable, Optional
+
+from riscv_check.checker.builder import CompileError
 
 from .checker.checker import IChecker, OptimizationLevel
 from .results_handler import IResultsHandler, Result
@@ -12,19 +14,31 @@ class Application:
     def __init__(
         self,
         logger: Logger,
+        compiler_error_logger: Logger,
         parser: ITestsParser,
         checker: IChecker,
         results_handler: IResultsHandler,
         optimization_levels: Iterable[OptimizationLevel],
     ):
         self.logger = logger
+        self.compiler_error_logger = compiler_error_logger
         self.parser = parser
         self.checker = checker
         self.results_handler = results_handler
         self.optimization_levels = optimization_levels
 
-    async def __run_test(self, test: Test, opt_level: OptimizationLevel) -> Result:
-        passed = await self.checker.check(test, opt_level)
+    async def __run_test(self, test: Test, opt_level: OptimizationLevel) -> Optional[Result]:
+        try:
+            passed = await self.checker.check(test, opt_level)
+        except CompileError as e:
+            self.logger.warning(
+                f"Failed to compile test '{test.name}' "
+                f"for {test.instruction}, {opt_level.name}"
+            )
+            self.compiler_error_logger.warning(
+                f"'{test.name}' for {test.instruction}, {opt_level.name}:\n{e}"
+            )
+            return None
 
         self.logger.info(
             f"Test '{test.name}' for {test.instruction}, {opt_level.name} has "
@@ -34,13 +48,14 @@ class Application:
         return Result(test=test, opt_level=opt_level, passed=passed)
 
     async def __run_tests(self, tests: Iterable[Test]) -> list[Result]:
-        return await asyncio.gather(
+        results: list[Optional[Result]] = await asyncio.gather(
             *(
                 self.__run_test(test, opt_level)
                 for test in tests
                 for opt_level in self.optimization_levels
             )
         )
+        return [r for r in results if r is not None]
 
     def run(self) -> None:
         tests = self.parser.parse_tests()
